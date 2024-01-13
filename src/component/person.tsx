b23af104787547f072758c546f5e871eaab320f3
JetShen <fernandoarielthielelepe@gmail.com>
2024-01-13 15:22:46 -0300

    Update person component with new image URL

--- src/component/person.tsx
+++ src/component/person.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image'
+const ulrTest = 'https://img.freepik.com/premium-photo/anime-girl-shark-costume-holding-stuffed-animal-generative-ai_958124-30525.jpg'
 
-export default function Person(){
+export default function Person() {
     return (
         <li className="person">
-            <Image src="" alt="cat.jpg" className="profileFollowImg" />
+            <Image src={ulrTest}
+                alt="Picture of the author"
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: '50%', height: 'auto' }}
+                className='profilePostImg' />
             <div className="section">
-            <h4 className="name">Name</h4>
-            <p className="username">@username</p>
+                <h4 className="name">Name</h4>
+                <p className="username">@username</p>
             </div>
         </li>
     )
