ee4d88bc6b72ece871956f8fed3a72c0474df307
JetShen <fernandoarielthielelepe@gmail.com>
2024-01-13 14:45:17 -0300

    Add image domain to next.config.js and update Post component styles

--- src/component/Post.tsx
+++ src/component/Post.tsx
@@ -1,22 +1,32 @@
 import '@/styles/post.css';
 import Image from 'next/image'
 
+const ulrTest = 'https://img.freepik.com/premium-photo/anime-girl-shark-costume-holding-stuffed-animal-generative-ai_958124-30525.jpg'
+
 export default function Post({i}:{i:number}) {
 
     return(
         <div className="PostObject">
-          <div className="miniSection">
-            <Image src="" alt="cat.jpg" className="profilePostImg" />
-            <div className='section'>
-              <h5 className="name">Name</h5>
-              <p className="username">@username</p>
+            <div className="PostImage">
+                <Image src={ulrTest} 
+                  alt="Picture of the author"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: '100%', height: 'auto' }}   
+                  className='profilePostImg' />
             </div>
-          </div>
-          <div className="innerContent">
-            {Array.from({ length: i }).map((_, j) => (              
-              <p key={j} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla ipsum ipsam voluptatibus harum architecto voluptate nihil similique. Cumque fugit quos, autem ab sed atque. Laboriosam repudiandae nisi fugit qui?</p>
-            ))}
-          </div>
+            <div className="PostContent">
+                <div className="minisection">
+                  <strong className="username">UserName</strong>
+                  <p className="userid">@username</p>
+                </div>
+                <div className="innerContent">
+                {Array.from({ length: i }).map((_, j) => (              
+                  <p key={j} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla ipsum ipsam voluptatibus harum architecto voluptate nihil similique. Cumque fugit quos, autem ab sed atque. Laboriosam repudiandae nisi fugit qui?</p>
+                ))}
+                </div>
+            </div>
         </div>
     );
 }
