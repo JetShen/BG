16aa9a3e78d29c158aac2c026f0c486c7fe15277
JetShen <fernandoarielthielelepe@gmail.com>
2024-01-15 00:49:45 -0300

    aaaaaaaaaa

--- src/app/api/searchP/route.ts
+++ src/app/api/searchP/route.ts
@@ -1,12 +1,13 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
+
 export async function GET() {
 
     try {
-        const result = await sql`SELECT Post.PostId, Post.Content, UserTest.UserId, UserTest.Name, UserTest.Username
+        const result = await sql`SELECT Post.PostId, Post.Content, Usuario.UserId, Usuario.Name, Usuario.Username
         FROM Post
-        JOIN UserTest ON Post.UserId = UserTest.UserId`;
+        JOIN Usuario ON Post.UserId = Usuario.UserId`;
         return NextResponse.json({ result }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,42 +1,52 @@
-'use client'
-import Post from '@/component/Post'
-import { useState, useEffect } from 'react';
+'use client';
+import { useQuery, useMutation, QueryClientProvider, QueryClient, useQueryClient} from '@tanstack/react-query';
+import { useState } from 'react';
+import Post from '@/component/Post';
 import { PostType } from '@/type/post';
 
-export default function Home() {
+
+const queryClient = new QueryClient()
+
+export default function App(){
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  )
+}
+
+
+function Home() {
   const [ContentData, setContentData] = useState<string>('');
-  const [Posts, setPosts] = useState<PostType[]>([]);
+  const queryClient = useQueryClient()
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const posts = await getPosts();
-      setPosts(posts);
-      console.log(posts);
-    }
-    fetchData();
-  }, []);
-
-  async function getPosts() {
-    try {
-      const res = await fetch('/api/searchP');
-      const data = await res.json();
-      if (data && data.result) {
-        return data.result.rows;
-      } else {
-        console.error("Unexpected response format:", data);
-      }
-    } catch (error) {
-      console.error("Error fetching data:", error);
+  
+// Define una función para obtener los posts desde la API
+  async function fetchPosts() {
+    console.log("fetching posts")
+    const res = await fetch('/api/searchP',{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    const data = await res.json();
+    if (data && data.result) {
+      return data.result.rows;
+    } else {
+      throw new Error("Unexpected response format");
     }
   }
-  
 
-  async function MakePost(event: any) {
-    event.preventDefault();
+  // Utiliza useQuery para obtener los 
+  const query = useQuery({ queryKey: ['post'], queryFn: fetchPosts })
+
+  async function MakePostMutated(event: any) {
     const PostObject = {
       content: ContentData,
       userid: 1,
     }
+    console.log(PostObject);
 
     const res = await fetch('/api/post', {
       method: 'POST',
@@ -45,42 +55,50 @@ export default function Home() {
       },
       body: JSON.stringify(PostObject)
     });
+    console.log('API Response:', res);
+  }
 
-    if (res.ok) {
-      setContentData('');
-    } else {
-      console.error('Error al realizar la solicitud:', res.statusText);
-    }
+  const mutation = useMutation({
+    mutationFn: MakePostMutated,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ['post'] })
+    },
+  })
 
+  function makePost(event: any) {
+    event.preventDefault();
+    mutation.mutate({ content: ContentData, userid: 1 });
   }
 
   function update(event: any) {
     setContentData(event.target.value);
   }
 
-  
-
   return (
     <>
-      <form onSubmit={MakePost} className="makePost">
-        <input contentEditable={true} className='PostArea' placeholder='Make a Post' onChange={update}></input>
+      <form onSubmit={makePost} className="makePost">
+        <input
+          contentEditable={true}
+          className="PostArea"
+          placeholder="Make a Post"
+          onChange={update}
+          value={ContentData}
+        />
         <div className="PostOptions">
-          <button type='submit'>Post</button>
+          <button type="submit">Post</button>
         </div>
       </form>
-      {Posts.map((post: PostType) => {
-        return (
-          <Post
-            key={post.postid}
-            name={post.name}
-            username={post.username}
-            content={post.content}
-            postid={post.postid}
-            userid={post.userid}
-          />
-        )
-        })
-      }
+      {query.data?.map((post: PostType) => (
+        <Post
+          key={post.postid}
+          name={post.name}
+          username={post.username}
+          content={post.content}
+          postid={post.postid}
+          userid={post.userid}
+        />
+      ))}
     </>
-  )
+  );
 }
