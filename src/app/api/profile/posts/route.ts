9a7c80fb4fa68468890fcbc9d7bbd7a081e49f60
JetShen <fernandoarielthielelepe@gmail.com>
2024-01-20 22:51:48 -0300

    Add pagination and infinite scrolling to profile posts page

--- src/app/api/profile/posts/route.ts
+++ src/app/api/profile/posts/route.ts
@@ -9,7 +9,13 @@ export async function GET(request: NextRequest){
     try {
         const client = await GetClient();
         const cursor = request.nextUrl.searchParams.get("cursor")
+        const userid = request.nextUrl.searchParams.get("userid")
+        if (userid === undefined || userid === null) {
+            return NextResponse.json({ error: 'Missing "userid" parameter' }, { status: 500 });
+        }
+        const useridValue = parseInt(userid, 10);
         console.log('cursor: ', cursor);
+        console.log('userid: ', useridValue);
 
         if (cursor === undefined || cursor === null) {
             return NextResponse.json({ error: 'Missing "cursor" parameter' }, { status: 500 });
@@ -27,9 +33,10 @@ export async function GET(request: NextRequest){
             SELECT post.postId, post.Content, user.UserId, user.Name, user.Username
             FROM post
             JOIN user ON post.UserId = user.UserId
+            WHERE post.UserId = ?
             order by post.postId desc
             LIMIT ? OFFSET ?
-        `, [pageSize, pageParam]);
+        `, [useridValue, pageSize, pageParam]);
         const posts: Array<PostType> = result[0] as Array<PostType>;
         const len = Object.keys(posts);
         const ln: number = len.length;
