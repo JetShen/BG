392126f8d9db326c3468133e4d52f87ef31617b9
JetShen <fernandoarielthielelepe@gmail.com>
2024-01-29 23:15:52 -0300

    Refactor getPostById function to recursively fetch parent posts

--- src/app/api/post/getOne/route.tsx
+++ src/app/api/post/getOne/route.tsx
@@ -2,38 +2,52 @@ import { GetClient } from "@/client/mysql";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
+async function getPostById(postId: number, client: any, posts: any[] = []) {
+    // console.log(posts)
+    const result = await client.query(
+        `SELECT 
+            post.PostID,
+            post.Content,
+            post.ParentPostId,
+            user.UserID,
+            user.Name,
+            user.Username,
+            COUNT(likes.LikeID) AS cantidad_likes
+        FROM 
+            post
+        JOIN 
+            user ON post.UserID = user.UserID
+        LEFT JOIN 
+            likes ON post.PostID = likes.PostID
+        WHERE 
+            post.PostID = ?
+        GROUP BY 
+            post.PostID, post.Content, post.ParentPostId, user.UserID, user.Name, user.Username
+        `, [postId]);
+
+    const post = result[0];
+
+    if (post[0] && post[0].ParentPostId !== null) {
+        posts.splice(0, 0, post[0]); 
+        return getPostById(post[0].ParentPostId, client, posts);
+    } else {
+        posts.splice(0, 0, post[0]); 
+        return posts;
+    }
+}
 
 export async function GET(request: NextRequest){
     try {
         const client = await GetClient();
-        const postid  = request.nextUrl.searchParams.get("postId");
-        const numberPost = Number(postid);
+        const postId = Number(request.nextUrl.searchParams.get("postId"));
 
         if (!client) {
             return NextResponse.json({ error: 'Database Connection Failed!' }, { status: 500 });
         }
 
-        const result = await client.query(
-            `SELECT 
-                post.PostID,
-                post.Content,
-                user.UserID,
-                user.Name,
-                user.Username,
-                COUNT(likes.LikeID) AS cantidad_likes
-            FROM 
-                post
-            JOIN 
-                user ON post.UserID = user.UserID
-            LEFT JOIN 
-                likes ON post.PostID = likes.PostID
-            WHERE post.PostID = ?
-            GROUP BY 
-                post.PostID, post.Content, user.UserID, user.Name, user.Username
-            `, [numberPost]);
+        const posts = await getPostById(postId, client);
 
-        const post = result[0];
-        return NextResponse.json({ post }, { status: 200, headers: { 'Content-Type': 'application/json' } });
+        return NextResponse.json({ post:posts }, { status: 200, headers: { 'Content-Type': 'application/json' } });
         
     } catch (error: any) {
         console.error('Error:', error);
